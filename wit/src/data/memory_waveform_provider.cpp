988dfca6c764e15812a788cb373bc9ba5ed7c894
intwisty <intmainwist@gmail.com>
2026-08-20 17:07:47 +0900

    Fix: MemoryWaveformProviderのモノラル対応

--- wit/src/data/memory_waveform_provider.cpp
+++ wit/src/data/memory_waveform_provider.cpp
@@ -10,8 +10,7 @@
 
 namespace wit {
 
-MemoryWaveformProvider::MemoryWaveformProvider(std::vector<uint8_t>&& pcm_data,	 uint64_t sample_count,
-                                               SampleFormat	  sample_format, uint16_t bit_depth, uint8_t  channels) noexcept
+MemoryWaveformProvider::MemoryWaveformProvider(std::vector<uint8_t>&& pcm_data, uint64_t sample_count, SampleFormat sample_format, uint16_t bit_depth, uint8_t channels) noexcept
     : pcm_data_(std::move(pcm_data)),
       sample_count_(sample_count),
       sample_format_(sample_format),
@@ -29,24 +28,37 @@ uint64_t MemoryWaveformProvider::Read(uint64_t sample_offset, uint64_t sample_co
     const uint32_t bytes_per_sample = static_cast<uint32_t>(bit_depth_) / 8u;
     const uint32_t bytes_per_frame  = bytes_per_sample * channels_;
 
-	/// HACK: 1-channel is out of scope during the beta phase
-	if (out_right == nullptr) return 0;
+	// 16bit int
 	if (sample_format_ == SampleFormat::INT && bit_depth_ == 16) {
 		for (uint32_t i = 0; i < to_read; ++i) {
 			const uint64_t frame_index = sample_offset + i;
 			const uint8_t* fp          = pcm_data_.data() + frame_index * bytes_per_frame;
 
-			out_left[i]  = decode::ReadInt16LE(fp);
-			out_right[i] = decode::ReadInt16LE(fp + 2);
+			if (channels_ == 1) {
+					const float s	= decode::ReadInt16LE(fp);
+					out_left[i]		= s;
+					out_right[i]	= s;
+			} else if (channels_ == 2) {
+					out_left[i]  = decode::ReadInt16LE(fp);
+					out_right[i] = decode::ReadInt16LE(fp + 2);
+			}
 		}
 	}
+
+	// 32bit float
 	if (sample_format_ == SampleFormat::FLOAT && bit_depth_ == 32) {
 		for (uint32_t i = 0; i < to_read; ++i) {
 			const uint64_t frame_index = sample_offset + i;
 			const uint8_t* fp          = pcm_data_.data() + frame_index * bytes_per_frame;
 
-			out_left[i]  = decode::ReadFloat32LE(fp);
-			out_right[i] = decode::ReadFloat32LE(fp + 4);
+			if (channels_ == 1) {
+				const float s	= decode::ReadFloat32LE(fp);
+				out_left[i]		= s;
+				out_right[i]	= s;
+			} else if (channels_ == 2) {
+				out_left[i]  = decode::ReadFloat32LE(fp);
+				out_right[i] = decode::ReadFloat32LE(fp + 4);
+			}
 		}
 	}
 
